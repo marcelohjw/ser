0f076d50f2fcb06ac139d68775ab6d92065c3a4d
Marcelo Júnior <marcelohjw@gmail.com>
2021-07-05 17:58:57 -0300

    Quadrilhas V1 finished

--- quadrilhas.c
+++ quadrilhas.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-     int i, j, quadrilhas, notas;
+     int i, j, quadrilhas;
+     int notas[3] = {6, 3, 2};
      printf("Quantas quadrilhas irão participar?\n");
      scanf("%d", &quadrilhas);
      for (i = 0; i < quadrilhas; i++)
      {
-         for(int i=0; i<2; i++) {
-            printf("-------------\n");
-            printf("Nota\n");
-         }
-         printf("\n");
+        printf("Quadrilha %d ", i+1);
+        printf("-------------\n");
+        for (j = 0; j <= 2; j++) {
+            printf("Nota Jurado ");
+            printf("%d = %d\n", j+1, notas[j]);
+        }
+        printf("\n");
      }
      return 0;
  }
