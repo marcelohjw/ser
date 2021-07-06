9ac5c90e6621d09ac3397dff9650f0343643a6bb
Marcelo Júnior <marcelohjw@gmail.com>
2021-07-06 08:31:04 -0300

    Quadrilhas problem solved

--- quadrilhas.c
+++ quadrilhas.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-     int i, j, quadrilhas;
-     int notas[3] = {6, 3, 2};
-     printf("Quantas quadrilhas irão participar?\n");
+     int i, j, quadrilhas, vencedor;
+     float nota0, nota1, nota2, media, resu;
      scanf("%d", &quadrilhas);
      for (i = 0; i < quadrilhas; i++)
      {
-        printf("Quadrilha %d ", i+1);
-        printf("-------------\n");
-        for (j = 0; j <= 2; j++) {
-            printf("Nota Jurado ");
-            printf("%d = %d\n", j+1, notas[j]);
+        scanf("%f %f %f", &nota0, &nota1, &nota2);
+        media = (nota0 + nota1 + nota2) / 3;
+        //printf("Média Quadrilha %d = %.2f\n", i+1, media);
+        if(media > resu) {
+            resu = media;
+            vencedor = i + 1;
         }
-        printf("\n");
      }
+     printf("Vencedor:%d\n", vencedor);
+     printf("Nota:%.2f", resu);
      return 0;
  }
