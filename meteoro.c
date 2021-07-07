726f0982412e4099d6566a90a039bfb5b269f0a0
Marcelo Júnior <marcelohjw@gmail.com>
2021-07-07 08:57:11 -0300

    Study finished but not solved

--- meteoro.c
+++ meteoro.c
@@ -2,12 +2,24 @@
 
 int main(void) {
 
-    int i, horas;
-    scanf("%d", &horas);
+    int i, linhas, hora, temperatura_vencedora;
+    float t1, t2, diferenca_past, diferenca;
+    diferenca = 0;
 
-    for(i = 0; i<horas; i++) {
-        printf("Hora %d\n", i+1);
+    scanf("%d", &linhas);
+
+    for(i = 1; i<=linhas; i++) {
+        scanf("%d %f %f", &hora, &t1, &t2);
+        diferenca = t1 - t2;
+        //printf("%f", diferenca);
+        if(diferenca > diferenca_past) {
+            temperatura_vencedora = hora;
+        }
+        diferenca_past = diferenca;
+        //printf("%f", diferenca_past);
     }
+    printf("%d", temperatura_vencedora);
+
 
     return 0;
 }
